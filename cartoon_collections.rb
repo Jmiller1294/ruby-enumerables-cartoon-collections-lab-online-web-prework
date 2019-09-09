7c4419849e88cd83d06059de02fec7262213110c
Justin Miller <jayman1294@gmail.com>
2019-09-09 06:00:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
-def roll_call_dwarves(array)# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
   
-  
+  dwarves.each_with_index |num, name|
   
 end
 
