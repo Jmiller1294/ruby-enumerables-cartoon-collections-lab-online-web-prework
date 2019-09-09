9d3576cf02eefe6f0ba13606350393b7a70ebfe2
Justin Miller <jayman1294@gmail.com>
2019-09-09 07:23:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,10 @@ def summon_captain_planet(calls)
   end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls)
   # Your code here
+  calls
+  
 end
 
 def find_the_cheese# code an argument here
