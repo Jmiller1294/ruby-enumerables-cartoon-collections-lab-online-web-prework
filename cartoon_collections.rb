bb9684f02e973724b3d6dab0024caa6a02c1d5a2
Justin Miller <jayman1294@gmail.com>
2019-09-09 07:11:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ end
 def summon_captain_planet(calls)
   # Your code here
   calls.collect do |powers| 
-    puts "#{powers.capitalize!}" + "!"
+    puts "#{powers.capitalize}" + "!"
     
   end
   calls
