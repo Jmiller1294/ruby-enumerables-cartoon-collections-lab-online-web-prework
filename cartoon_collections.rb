a5429507915ccffcc6bb2746302012ce1233f760
Justin Miller <jayman1294@gmail.com>
2019-09-09 06:47:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,12 +1,15 @@
-def roll_call_dwarves(dwarves)# code an argument here
+def roll_call_dwarves(dwarves)
   # Your code here
   dwarves.each_with_index do |item, index|
     puts "#{index + 1}." + "#{item}"
   end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(calls)
   # Your code here
+  calls.map do |elements| 
+    puts "#{elements}!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
