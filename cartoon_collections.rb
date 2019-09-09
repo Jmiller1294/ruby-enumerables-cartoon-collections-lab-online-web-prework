f1ed2d58d257914246298e7a6d998eb209cf4eec
Justin Miller <jayman1294@gmail.com>
2019-09-09 06:42:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ def roll_call_dwarves(dwarves)# code an argument here
       puts "#{index + 1}." + "#{item}"
   end
 end
-
+p 
 def summon_captain_planet# code an argument here
   # Your code here
 end
