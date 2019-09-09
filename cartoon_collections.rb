d41dd06d82a36cee058e98957370549a262c7f92
Justin Miller <jayman1294@gmail.com>
2019-09-09 06:27:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,10 +3,10 @@ def roll_call_dwarves(dwarves)# code an argument here
   
   dwarves.each_with_index do |item, index|
    
-      puts "#{index}." "#{item}"
+       "#{index}." "#{item}"
   
   end
-  
+  puts dwarves
 end
 
 def summon_captain_planet# code an argument here
