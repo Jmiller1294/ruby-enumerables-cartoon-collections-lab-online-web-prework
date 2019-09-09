e61837c45eeea35358e97bd7d880bb3e6f1d58d3
Justin Miller <jayman1294@gmail.com>
2019-09-09 08:28:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,7 +27,7 @@ end
 
 def find_the_cheese(array)
   array.find do |cheese|
-    if cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
+    cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
   end
 end
 
